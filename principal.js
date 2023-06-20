2d14eb998d70e441d3e7f5f1a292c00000e86993
Eljovenpatamuslo <92558330+Eljovenpatamuslo@users.noreply.github.com>
2023-06-20 17:05:29 -0300

    cuando sos admin te aparece para ver las tablas de usuario

--- principal.js
+++ principal.js
@@ -5,6 +5,16 @@ $.ajax({
         let admin = JSON.parse(response);
         if(admin == null){
              window.location = 'logearse.html';
+        }else if(admin == 1){
+            let admin1 = JSON.parse(response);
+            var paragraph = document.getElementById("admin");
+            if (admin1 != null){
+                let template = '';
+                template += `Para ver la lista de usuarios registrados anda <a href="usuarios.html">aca</a>`;
+                $('#admin').html(template);
+            }
+        
+            paragraph.appendChild(text);
         }
     },
     error: function (jqXHR, exception) {
@@ -36,7 +46,7 @@ $(document).ready(function () {
             let nombre = JSON.parse(response);
             var paragraph = document.getElementById("pepe");
             if (nombre != null){
-                var text = document.createTextNode("Bienvenido "+nombre);
+                var text = document.createTextNode(nombre);
             }
         
             paragraph.appendChild(text);

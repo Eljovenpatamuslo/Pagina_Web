81d31699b7fd88c1a8430f0cbfb8db3a1dffc1b9
Eljovenpatamuslo <92558330+Eljovenpatamuslo@users.noreply.github.com>
2023-07-04 14:36:13 -0300

    deleted admin

--- principal.js
+++ principal.js
@@ -12,10 +12,8 @@ $(document).ready(function () {
             
             let template = `<img width="50" height="50" style="border-radius: 25px" src="Images/Users/${data.picture}">`;
             let template2 = `<div class="texto_login">${data.user}</div>`;
-            let template3 = `<a href="usuarios.html" class="link texto_login">lista de usuarios</a>`;
             $('#foto').html(template);
             $('#user').html(template2);
-            $('#admin').html(template3);
         },
         error: function (jqXHR, exception) {
             console.log(jqXHR);
